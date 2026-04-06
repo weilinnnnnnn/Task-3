bd073d15d474b4e18be008a425a5743c4bb66187
weilinnnnnnn <terweilin31@gmail.com>
2026-04-06 16:20:52 +0200

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,5 @@
+pip install reportlab --break-system-packages
+python3 create_pdf.py
+python3 pdf.py file1.pdf file2.pdf
+md5sum collision1.pdf collision2.pdf
+sha256sum collision1.pdf collision2.pdf
